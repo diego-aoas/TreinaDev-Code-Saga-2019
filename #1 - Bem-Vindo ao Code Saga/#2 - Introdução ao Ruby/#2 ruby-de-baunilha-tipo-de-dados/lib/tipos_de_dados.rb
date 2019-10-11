af8da645d2fc0968622b8056dc08de7015cb63ad
Diego <55292470+diego-aoas@users.noreply.github.com>
2019-10-10 21:00:52 -0300

    Add files via upload

---
+++ #1 - Bem-Vindo ao Code Saga/#2 - Introdução ao Ruby/#2 ruby-de-baunilha-tipo-de-dados/lib/tipos_de_dados.rb
@@ -0,0 +1,33 @@
+class TiposDeDados
+  def ola_mundo
+    'Olá Mundo'
+  end
+
+  def um_numero
+    10
+  end
+
+  def um_decimal
+    12.5
+  end
+
+  def verdadeiro?
+    1 < 2
+  end
+
+  def falso?
+    10 > 5 && 1 > 6
+  end
+
+  def array_vazio
+    array = []
+  end
+
+  def array_com_um_elemento
+    array = [10]
+  end
+
+  def array_com_dois_elementos
+    array = [0,1]
+  end
+end
